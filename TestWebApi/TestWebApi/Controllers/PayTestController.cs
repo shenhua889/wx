9feda49e13513cfb740a519dd6e73e82a9f2a5dc
shenhua889 <shenhua889@126.com>
2018-05-10 18:13:30 +0800

    1.01

--- TestWebApi/TestWebApi/Controllers/PayTestController.cs
+++ TestWebApi/TestWebApi/Controllers/PayTestController.cs
@@ -10,10 +10,26 @@ namespace TestWebApi.Controllers
 {
     public class PayTestController : ApiController
     {
+        struct WxPayOpenId
+        {
+            public string appid;
+            public string secret;
+            public string loginCode;
+            public string grant_type;
+        };
         [HttpPost]
         public string GetLoginCode(JObject loginCode)
         {
             return loginCode["loginCode"].ToString();
+        }
+        private void GetOpenID(string loginCode)
+        {
+            WxPayOpenId wxpaypoenid;
+            wxpaypoenid.appid = "wxe59a859083fbcf32";
+            wxpaypoenid.secret = "838bd800db4f2120f87baace119a857d";
+            wxpaypoenid.loginCode = loginCode;
+            wxpaypoenid.grant_type = "authorization_code";
+
         }
         public string GetTest()
         {
